27a67fe56d4b3b1daf371a65cb0f3aacf7c827fe
Josué Garza <josuegarza.dev@gmail.com>
2024-01-18 17:18:19 -0600

    add resume and cv

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
 export const LINKS = {
   github: "https://github.com/josuegarza42",
   linkedin: "https://www.linkedin.com/in/josue-garza/",
+  resume: "https://flowcv.com/resume/its72h4324",
+  cv: "https://drive.google.com/file/d/1UgChEhtSquO4Z-YFh6Y3E_7C4RaQFaqn/view?usp=sharing",
 
   // discord: "https://discordapp.com/users/163300027618295808",
 };
